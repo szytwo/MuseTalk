21722a092fa11dcdb1d7677a88ad8feaa94d5414
jsb-szy <szytwo@qq.com>
2024-12-18 23:00:55 +0800

    优化

--- custom/TextProcessor.py
+++ custom/TextProcessor.py
@@ -45,28 +45,29 @@ class TextProcessor:
     @staticmethod
     def log_error(exception: Exception, log_dir='error'):
         """
-        记录错误信息到指定目录，并按日期时间命名文件。
+        记录错误信息到指定目录，并按日期小时命名文件。
 
         :param exception: 捕获的异常对象
         :param log_dir: 错误日志存储的目录，默认为 'error'
         """
         # 确保日志目录存在
         os.makedirs(log_dir, exist_ok=True)
+        # 获取当前日期和小时，作为日志文件名的一部分
+        timestamp_hour = datetime.datetime.now().strftime('%Y-%m-%d_%H')  # 到小时
         # 获取当前时间戳，格式化为 YYYY-MM-DD_HH-MM-SS
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
         # 创建日志文件路径
-        log_file_path = os.path.join(log_dir, f'error_{timestamp}.log')
+        log_file_path = os.path.join(log_dir, f'error_{timestamp_hour}.log')
         # 从 exception 获取堆栈信息
         if exception.__traceback__:
             error_traceback = ''.join(traceback.format_exception(type(exception), exception, exception.__traceback__))
         else:
             error_traceback = "无法获取堆栈信息"
-
-        # 写入错误信息到文件
-        with open(log_file_path, 'w') as log_file:
+        # 写入错误信息到文件，使用追加模式 'a'
+        with open(log_file_path, 'a') as log_file:
             log_file.write(f"错误发生时间: {timestamp}\n")
             log_file.write(f"错误信息: {str(exception)}\n")
             log_file.write("堆栈信息:\n")
             log_file.write(error_traceback + '\n')
-        
+
         logging.info(f"错误信息已保存至: {log_file_path}")
